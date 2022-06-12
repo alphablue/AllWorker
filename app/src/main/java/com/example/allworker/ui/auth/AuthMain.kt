03a6ed33b464a1451a652b6efb2b11b83e6ec71c
Jin <imy123123@naver.com>
2022-06-13 08:41:59 +0900

    regex 오류 수정중

--- app/src/main/java/com/example/allworker/ui/auth/AuthMain.kt
+++ app/src/main/java/com/example/allworker/ui/auth/AuthMain.kt
@@ -1,30 +1,27 @@
+@file:Suppress("OPT_IN_IS_NOT_ENABLED")
+
 package com.example.allworker.ui.auth
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusOrder
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.allworker.util.TextUtil
 
 @OptIn(ExperimentalComposeUiApi::class)
@@ -34,7 +31,11 @@ fun AuthMain() {
     val keyboardController = LocalSoftwareKeyboardController.current
 
     Column(
-        verticalArrangement = Arrangement.spacedBy(12.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
         EmailField(focusRequester = focusRequester)
@@ -53,10 +54,14 @@ fun EmailField(
     val (localFocusRequester) = FocusRequester.createRefs()
 
     TextField(
-        modifier = Modifier.focusRequester(localFocusRequester),
+        modifier = Modifier
+            .focusRequester(localFocusRequester)
+            .fillMaxWidth(0.8f),
         value = email,
         onValueChange = {email = it},
         label = { Text("Email")},
+        placeholder = { Text("example@gmail.com") },
+        singleLine = true,
         trailingIcon = {
             if(email.isBlank().not())
                 Icon(
@@ -97,17 +102,40 @@ fun PasswordField(
 
     TextField(
         value = password,
+        singleLine = true,
+        placeholder = { Text(text = "영문자, 숫자 조합 6자리 이상 13자리 이하", fontSize = 10.sp)},
         onValueChange = {password = it},
-        modifier = Modifier.focusRequester(focusRequester),
+        modifier = Modifier
+            .focusRequester(focusRequester)
+            .fillMaxWidth(0.8f),
         label = { Text(text = "password")},
         visualTransformation = PasswordVisualTransformation(),
+        trailingIcon = {
+            if(password.isBlank().not())
+                Icon(
+                    imageVector = Icons.Default.Clear,
+                    contentDescription = "clear text",
+                    modifier = Modifier
+                        .size(12.dp)
+                        .clickable {
+                            password = ""
+                            focusRequester.requestFocus()
+                            keyboardController?.show()
+                        }
+                )
+        },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password, imeAction = ImeAction.Done),
         keyboardActions = KeyboardActions(
             onDone = {
                 isValidPassword = TextUtil.isValidPassword(password)
 
-                if (isValidPassword)
-                    keyboardController?.hide()}
+                if (isValidPassword) {
+                    keyboardController?.hide()
+                }
+                else {
+                    password = ""
+                }
+            }
         )
     )
 
@@ -116,4 +144,6 @@ fun PasswordField(
             text = "비밀번호는 영문 소문자 숫자를 혼합하여 6자리 이상 13자리 이하여야 합니다.",
             color = MaterialTheme.colors.error
         )
+    else
+        Text(text = "통과하였습니다.")
 }
